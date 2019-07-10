7ca8a93682b9f90819eca7534b91d9efe54a2754
Guansir28 <642175604@qq.com>
2019-07-10 15:00:52 +0800

    iss53继续开发,添加了11111111提交

--- src/main/java/com/yidu/TestIss53.java
+++ src/main/java/com/yidu/TestIss53.java
@@ -4,5 +4,9 @@ public class TestIss53 {
 
 	public static void main(String[] args) {
 		System.out.println("iss53分支下的新功能");
+		
+		//---------修复bug后继续开发------------------------------
+		System.out.println("111111111111111111111111");
+		
 	}
 }
